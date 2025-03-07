be179263ecc01aac824b72c4550fee63a968f8f9
Miguel Rodrigues <miguel.b.rodrigues@gmail.com>
2025-03-07 15:01:18 +0000

    initial commit

---
+++ install.md
@@ -0,0 +1,11 @@
+# Permissions
+1.  Set-ExecutionPolicy -ExecutionPolicy RemoteSigned -Scope CurrentUser
+1. High priority for ollama server
+
+# Instalation
+1.  python -m venv myenv
+1. cd .\myenv\Scripts\
+1. activate
+1. cd ../..
+1.  python -m pip install --upgrade pip
+1. pip install -r  .\requirements.txt
---
+++ test1.py
@@ -0,0 +1,41 @@
+from ollama import Client
+import json
+import time
+
+# Common definitions
+G_NRequest = 1
+G_cur_host_port = "http://localhost:11434"
+
+module_list = [
+ #'llama3.2',
+ #'lama2-uncensored:latest',
+ #'phi4-mini',
+  'deepseek-r1'
+]
+
+def timmings(NRequest, cur_model, cur_host_port):
+    """
+    Display timming to access local ollama server
+    """
+    start_time = time.time()
+    client = Client(
+      host=cur_host_port,
+      headers={'x-some-header': 'some-value'}
+    )
+    warm_time = time.time()
+    for i in range(0,NRequest):
+        response = client.chat(model=cur_model, messages=[
+          {
+            'role': 'user',
+            'content': 'Why is the sky blue?. reply in markdown'
+            ,
+          },
+        ])
+    end_time = time.time()
+    print(f"(Model{cur_model}-Warm up Time : {warm_time-start_time:.6f} seconds")
+    print(f"(Model{cur_model}-Query Time : {end_time-warm_time:.6f} seconds")
+    print(f"(Model{cur_model}-Average Query Time for {NRequest} Requests: {(end_time-warm_time)/NRequest} seconds")
+    print(f"(Model{cur_model}-Total  Query Time for {NRequest} Requests: {(end_time-warm_time)} seconds")
+
+for G_cur_model in module_list:
+    timmings(NRequest=G_NRequest, cur_model=G_cur_model, cur_host_port=G_cur_host_port)
